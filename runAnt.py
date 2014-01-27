5ccf503cd95024dd21595f47e5b89ebb317607bd
Jilang Miao <jlmiao@mit.edu>
2014-01-26 21:03:24 -0500

    finish shuffle pictures and remove useless Ant classes

--- runAnt.py
+++ runAnt.py
@@ -1,15 +1,8 @@
-"""
-6.177 Problem Set (IAP 2014)
-Completed by Jilang Miao (jlmiao@mit.edu)
-"""
-
-
-# Algorithm described at http://en.wikipedia.org/wiki/Langton%27s_ant
-
+# 1 - Import library
 import pygame, sys, random
-#import tests as T
 
 ### Global Variables
+
 WIDTH = 75  # this is the width of an individual square
 HEIGHT = 75 # this is the height of an individual square
 
@@ -20,6 +13,70 @@ white = (255, 255, 255)
 green = (0, 255, 0)
 red   = (255, 0, 0)
 blue  = (0, 0, 255)
+
+
+
+def construct_pic_array(size, numImage):
+    sizeArray = size[0]*size[1]
+    remainder = sizeArray/2
+    images = []
+    picVector = []
+    batchSequence = range(numImage)
+    while(remainder != 0):
+        batchSize = random.randint(1,min(numImage,remainder))
+        batchIndex= random.sample(batchSequence, batchSize)
+        for i in range(batchSize):
+            picVector.append(batchIndex[i])
+            picVector.append(batchIndex[i])
+        remainder = remainder - batchSize
+    sequence = range(sizeArray)
+    index = random.sample(sequence, sizeArray)
+    picArray = [[picVector[index[size[0]*row+col]] for col in range(size[1])] for row in range(size[0])]
+    return picArray
+
+
+def new_game():
+
+# 2 - Initialize the game
+
+    pygame.init()
+    pygame.mixer.init()
+    pygame.font.init()
+
+# 3 - Load images and audio
+
+# 3.1 - Load audio
+    pygame.mixer.music.load('images/doudizhu.mp3')
+    pygame.mixer.music.play(-1, 0.0)
+    pygame.mixer.music.set_volume(0.25)
+
+#3.2 - Load images
+    grass = pygame.image.load("images/grass.png")
+    background = pygame.image.load('images/background.jpg')
+    numImage = 8
+    images = []
+    for i in range(0,numImage):
+        images.append(pygame.image.load("images/"+str(i+1)+".jpg"))
+    images.append(pygame.image.load("images/back.jpg"))
+    images.append(pygame.image.load("images/background.jpg"))
+
+    size = (6,6)
+    picArray = construct_pic_array(size, numImage)
+
+
+    window_size = [size[1] * WIDTH + 200, size[0] * HEIGHT + 20] # width, height
+    screen = pygame.display.set_mode(window_size)
+
+    pygame.display.set_caption("Memory Card") # caption sets title of Window 
+
+    board = Board(size, picArray, images)
+
+    moveCount = 0
+
+    clock = pygame.time.Clock()
+
+    main_loop(screen, board, moveCount, clock, False, False)
+
 
 def get_row_top_loc(rowNum, height = HEIGHT):
     """
@@ -52,33 +109,6 @@ def update_text(screen, message, size ):
     textRect.centery = textY
     screen.blit(text, textRect)
 
-def new_game():
-    """
-    Sets up all necessary components to start a new game
-    of Langton's Ant.
-    """
-#    isDefault = raw_input("Would you like to use the default board? (Y/N)\n")
-#    if isDefault == 'Y':
-    size = (10,10)
-#    else:
-#      rowNum = input('Please input row number:\n')
-#      colNum = input('Please input column number:\n')
-#      size = (rowNum, colNum)
-    pygame.init() # initialize all imported pygame modules
-
-    window_size = [size[1] * WIDTH + 200, size[0] * HEIGHT + 20] # width, height
-    screen = pygame.display.set_mode(window_size)
-
-    pygame.display.set_caption("Langton's Ant") # caption sets title of Window 
-
-    board = Board(size)
-
-    moveCount = 0
-
-    clock = pygame.time.Clock()
-
-    main_loop(screen, board, moveCount, clock, False, False)
-
 def draw_grid(screen, size):
     """
     Draw the border grid on the screen.
@@ -93,31 +123,11 @@ def draw_grid(screen, size):
       pygame.draw.line(screen, blue, pnt1, pnt2)
     pass
 
-def construct_pic_array(size, numImage):
-    sizeArray = size[0]*size[1]
-    remainder = sizeArray
-    images = []
-    picVector = []
-    batchSequence = range(numImage)
-    for i in range(0,numImage):
-        images.append(pygame.image.load("images/"+str(i+1)+".jpg"))
-    while(remainder != 0):
-        batchSize = random.randrange(1, min(numImage, remainder))
-        batchIndex= random.sample(batchSequence, batchSize)
-        for i in range(batchSize):
-            picVector.append(images[batchIndex[i]])
-            picVector.append(images[batchIndex[i]])
-        remainder = remainder - 2*batchSize
-    sequence = range(sizeArray)
-    index = random.sample(sequence, sizeArray)
-    picArray = [[picVector[size[0]*row+col] for col in range(size[1])] for row in range(size[0])]
-    return picArray
     
 # Main program Loop: (called by new_game)
 def main_loop(screen, board, moveCount, clock, stop, pause):
     board.squares.draw(screen) # draw Sprites (Squares)
     draw_grid(screen, board.size)
-    board.theAnt.draw(screen) # draw ant Sprite
     pygame.display.flip() # update screen
     
     if stop == True:
@@ -187,16 +197,17 @@ def main_loop(screen, board, moveCount, clock, stop, pause):
     pygame.quit() # closes things, keeps idle from freezing
 
 class Square(pygame.sprite.Sprite):
-    def __init__(self, row, col, pic)
+    def __init__(self, row, col, picIndex, image):
         pygame.sprite.Sprite.__init__(self)
         self.row = row
         self.col = col 
-        self.image = pygame.transform.scale(pic, (width, height))
+        self.image = pygame.transform.scale(image, (WIDTH, HEIGHT))
         self.rect = self.image.get_rect() # gets a rect object with width and height specified above
                                             # a rect is a pygame object for handling rectangles
         self.rect.x = get_col_left_loc(col)
         self.rect.y = get_row_top_loc(row)
-        self.color = color
+        self.picIndex = picIndex
+
 
     def get_rect_from_square(self):
         """
@@ -204,7 +215,7 @@ class Square(pygame.sprite.Sprite):
         """
         return self.rect;
         pass
-n
+
     def flip_color(self):
         """
         Flips the color of the square (white -> black or 
@@ -221,13 +232,13 @@ n
         self.image = pygame.image.load(fileName).convert_alpha()
    
 class Board:
-    def __init__(self, size, picArray):
+    def __init__(self, size, picArray,images):
 
         self.size = size
         
         #---Initializes Squares (the "Board")---#
         self.squares = pygame.sprite.RenderPlain()
-        self.boardSquares = [[Square(row,col,picArray[row][col] for col in range(size[1])] for row in range(size[0])]
+        self.boardSquares = [[Square(row,col, picArray[row][col], images[picArray[row][col]]) for col in range(size[1])] for row in range(size[0])]
         for row in range(size[0]):
           for col in range(size[1]):
             self.squares.add(self.boardSquares[row][col])
@@ -251,88 +262,6 @@ class Board:
         pass
 
 
-    def ant_get_square(self):
-        """ 
-        Rotate the ant, depending on the color of the square that it's on,
-        and returns the square that the ant is currently on
-        """
-        color = self.ant.get_current_square().color;
-
-        if(color is white):
-          self.ant.rotate_right()
-        else:
-          self.ant.rotate_left()
-        return self.get_square(self.ant.row, self.ant.col)
-        pass 
-
-class Ant(pygame.sprite.Sprite):
-    def __init__(self, board, col, row):
-        pygame.sprite.Sprite.__init__(self)
-        self.col = col
-        self.row = row
-        yourSquare = board.get_square(row,col)
-        self.rect = yourSquare.get_rect_from_square()
-#        center = self.rect.center
-#        self.rect = yourSquare.get_rect_from_square(center=center)
-        self.rotation = (0, 1) # pointing up
-        self.board = board
-        self.set_pic()
-        
-    def get_current_square(self):
-        return self.board.get_square(self.row, self.col)
-        pass
-        
-    def rotate_left(self):
-        """
-        Rotates the ant 90 degrees counterclockwise
-        """
-        self.image = pygame.transform.rotate(self.image, 90)        
-#        r0 = self.rotation[0]
-#        r1 = self.rotation[1]
-#        self.rotation[0] = r1
-#        self.rotation[1] =-r0
-        pass
-
-    def rotate_right(self):
-        """
-        Rotates the ant 90 degrees clockwise
-        """
-        self.image = pygame.transform.rotate(self.image,-90)
-#        r0 = self.rotation[0]
-#        r1 = self.rotation[1]
-#        self.rotation[0] =-r1
-#        self.rotation[1] = r0
-        pass
-    
-    def step_forward(self, board):
-        """
-        Make the ant take a step forward in whatever direction it's currently pointing.
-        Don't forget - row numbers increase from top to bottom and column numbers
-        increase from left to right!
-        """
-        r0 = self.rotation[0]
-        r1 = self.rotation[1]
-        color = self.get_current_square().color
-# movement is determined by the original color
-        if(color is black):
-          self.rotation = (r1, -r0)
-          self.row = (self.row + r0 + board.size[0])%board.size[0]
-          self.col = (self.col + r1 + board.size[1])%board.size[1]
-          self.rect = board.get_square(self.row,self.col).get_rect_from_square()
-        else:
-          self.rotation = (-r1, r0)
-          self.row = (self.row - r0 + board.size[0])%board.size[0]
-          self.col = (self.col - r1 + board.size[1])%board.size[1]
-          self.rect = board.get_square(self.row,self.col).get_rect_from_square()
-        pass
-    
-    def set_pic(self):
-        """
-        Sets the picture that represents our Ant.
-        If you want to use a new picture, you'll need to change
-        this method.
-        """
-        self.image = pygame.image.load("ant.png").convert_alpha()
 
 if __name__ == "__main__":
 
